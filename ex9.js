b1ad6c995fec9a8d770ba62d70d0b9036ea01a5f
Edu <edugacv@gmail.com>
2020-12-07 14:01:13 +0100

    Exercice 9 done

--- ex9.js
+++ ex9.js
@@ -6,10 +6,14 @@
  * Arreglalo para que la palabra "Ejecutar!" se muestre 10 veces por el terminal
  */
 
+ //Se nos quedava como bucle infinito porque nunca incrementabamos nuestro contador, y por tanto nuestra condición nuca se acabaría cumpliendo
+ //y crearíamos un bucle infinito. Simplemente hay que añadir i++ al final de nuestro while
+
 let n=10;
 let i=0;
 
 while(i<n) {
     console.log("Ejecutar!")
+    i++
 }
 
